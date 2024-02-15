db868efa87df5f4ca210c9e1bd45242ee1910d3f
shunma <shunmakajioka12@gmail.com>
2024-02-16 00:15:17 +0900

    apiからポケモンの画像とデータを出力するところまで

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import { getAllpokemon,getPokemon } from './utils/pokemon';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './Components/Card/Card';
 
 function App() {
+  
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const  [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemonData  = async () => {
+      //全てのデータを取得
+      let res = await getAllpokemon(initialURL);
+      //各ポケモンの詳細のデータを取得
+      loadpokemon(res.results);
+      setLoading(false);
+    }
+    fetchPokemonData();
+  }, [])
+
+
+  //各ポケモンのデータを取得するための関数
+  const loadpokemon = async(data) => {
+    let  _pokemonData =  await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return  pokemonRecord;
+      })
+    ) 
+    setPokemonData(_pokemonData);
+  }
+
+  console.log(pokemonData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='App'>
+     {loading ? (
+       <h1>loading中です</h1>
+     )
+    : (
+   <div className=' pokemonCardContainer'>
+     {pokemonData.map((pokemon ,index) => {
+       return <Card  key={index}  pokemon={pokemon}/>;
+     })}
+   </div>
+
+    )
+    }
+   </div>
   );
 }
 
