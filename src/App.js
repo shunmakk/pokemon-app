f5eae20e312c25d890d0ff87a1f5d82eea59de98
shunma <shunmakajioka12@gmail.com>
2024-02-18 16:48:53 +0900

    次のページと前のページに移動するためのロジックを追加

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
   const  [pokemonData, setPokemonData] = useState([]);
+  const  [nextURL, setNextURL] = useState('');
+  const  [prevURL, setprevtURL] = useState('');
 
   useEffect(() => {
     const fetchPokemonData  = async () => {
@@ -16,6 +18,13 @@ function App() {
       let res = await getAllpokemon(initialURL);
       //各ポケモンの詳細のデータを取得
       loadpokemon(res.results);
+      
+     //次のページのデータを取得
+     setNextURL(res.next);
+
+     //前のページのデータを取得
+     setprevtURL(res.previous);
+
       setLoading(false);
     }
     fetchPokemonData();
@@ -33,22 +42,56 @@ function App() {
     setPokemonData(_pokemonData);
   }
 
-  console.log(pokemonData)
+
+
+  //次のページと前のページを検索するボタンの関数を作成する
+  const handlePrevPage =  async  () => {
+    if(!prevURL) return;
+    setLoading(true)
+    let data = await getAllpokemon(prevURL);
+    await loadpokemon(data.results);
+    setNextURL(data.next);
+    setprevtURL(data.previous);
+    setLoading(false)
+  };
+  
+
+
+  const handleNextPage =  async   () => {
+   setLoading(true);
+   let data = await getAllpokemon(nextURL);
+   await loadpokemon(data.results);
+   setNextURL(data.next);
+   setprevtURL(data.previous);
+   setLoading(false);
+  };
+  
+
+
+
+
+
+
 
   return (
     <>
     <NavBar/>
    <div className='App'>
+   <div className='btn'>
+     <button onClick={handlePrevPage}>前へ</button>
+     <button onClick={handleNextPage}>次へ</button>
+   </div>
      {loading ? (
        <h1>loading中です</h1>
      )
     : (
+      <>
    <div className='pokemonCardContainer'>
      {pokemonData.map((pokemon ,index) => {
        return <Card  key={index}  pokemon={pokemon}/>;
      })}
    </div>
-
+   </>
     )
     }
    </div>
